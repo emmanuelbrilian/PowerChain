32ad6bdc6480db401e2a13bf6093161a8adfe75e
emmanuelbrilian <emmanuelbrilian@gmail.com>
2023-06-18 17:18:14 +0700

    FirstCommit

---
+++ myflaskapp/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from pymongo import MongoClient
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+
+app = Flask(__name__)
+
+# Koneksi ke MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['mydatabase']
+collection = db['users']
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password Do Not Match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# Route untuk halaman registrasi
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Memasukkan data pengguna ke dalam basis data MongoDB
+        user_data = {
+            'name': name,
+            'email': email,
+            'username': username,
+            'password': password
+        }
+        collection.insert_one(user_data)
+
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('register.html', form=form)
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
