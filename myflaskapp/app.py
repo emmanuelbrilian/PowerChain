29b5cdad58eb8de5cd294e973e316fea46f19c15
emmanuelbrilian <emmanuelbrilian@gmail.com>
2023-06-23 01:31:05 +0700

    app,py

--- myflaskapp/app.py
+++ myflaskapp/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 from pymongo import MongoClient
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -11,10 +12,12 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['mydatabase']
 collection = db['users']
 
+#Index
 @app.route('/')
 def index():
     return render_template('home.html')
 
+#About
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -52,6 +55,69 @@ def register():
         return redirect(url_for('index'))
 
     return render_template('register.html', form=form)
+
+#userlogin
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Mengambil data dari form
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Mencari pengguna berdasarkan username
+        user = collection.find_one({'username': username})
+
+        if user:
+            # Mendapatkan password yang disimpan
+            stored_password = user['password']
+
+            # Membandingkan password
+            if sha256_crypt.verify(password_candidate, stored_password):
+                #passed
+                session["logged_in"] = True
+                session["username"] = username
+
+                flash("You are now logged in", "success")
+                return redirect(url_for('dashboard'))
+                
+                # Login berhasil
+                # Lakukan tindakan yang diperlukan, seperti mengatur sesi atau mengarahkan pengguna ke halaman beranda
+            else:
+                error = "invalid login"
+                return render_template ("login.html", error=error)
+            
+
+        else:
+            error = "Username not found"
+            return render_template ("login.html", error=error)
+
+    return render_template('login.html')
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please login', 'danger')
+            return redirect (url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+def logout():
+        session.clear()
+        flash('You are now logged out', 'success')
+        return redirect(url_for('login'))
+
+# Dashboard
+@app.route ('/dashboard')
+@is_logged_in 
+def dashboard():
+    return render_template ('dashboard.html')
+    
+
 if __name__ == '__main__':
     app.secret_key='secret123'
     app.run(debug=True)
